7eac92e6b1355779e3bfd256b3ae1d66b1290349
ABSUBHISELVAM <arunsubhi369@gmail.com>
2025-03-03 12:10:45 +0530

    duck

--- facrorial.py
+++ facrorial.py
@@ -1,8 +1,8 @@
-def fuck(n):
+def duck(n):
     if n==0 | n==1:
         return 1
     else:
-        return n*fuck(n-1)
-n=int(input("Enter a fucking number"))
-print(f"The fucking factorial which makes you orgasm is : {fuck(n)}")
+        return n*duck(n-1)
+n=int(input("Enter a ducking number"))
+print(f"The ducking factorial which makes you orgasm is : {duck(n)}")
 
