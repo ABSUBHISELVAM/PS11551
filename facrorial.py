f302f7b4dbc247403ecbcd085b57cf550a70fde0
ABSUBHISELVAM <arunsubhi369@gmail.com>
2025-03-03 12:13:54 +0530

    changed the fucntion name to num

--- facrorial.py
+++ facrorial.py
@@ -1,8 +1,8 @@
-def duck(n):
+def num(n):
     if n==0 | n==1:
         return 1
     else:
-        return n*duck(n-1)
-n=int(input("Enter a ducking number"))
-print(f"The ducking factorial which makes you orgasm is : {duck(n)}")
+        return n*num(n-1)
+n=int(input("Enter a number"))
+print(f"The  factorial  is : {num(n)}")
 
